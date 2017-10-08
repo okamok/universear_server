53f165eece75069248f3fc0e5d8720b82958cf34
masahiro_mac <okamok108@gmail.com>
2017-10-08 15:58:55 +0900

    Sprite決済情報を更新

--- universear/dev_settings.py
+++ universear/dev_settings.py
@@ -20,8 +20,13 @@ SOCIAL_AUTH_TWITTER_SECRET = 'zodNRE2HNnaOQyQAzMyg9xPdA7UunVcVdXkElkTO4NaAwQYxya
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '110344682483-fqpd4fkdb42gtc0pf172f61s3gh2lid2.apps.googleusercontent.com'
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'pxJBS6A7F5qgiavEQNbr0qU8'
 
-STRIPE_API_KEY = 'sk_test_Po5fLfcGq5FnakXbyvB7IIO9'
-STRIPE_PUBLISHABLE_KEY = 'pk_test_Qf9hdfU6fy6sKc09jZ4hKH5T'
+# STRIPE 決済設定
+# STRIPE_PUBLISHABLE_KEY = 'pk_test_Qf9hdfU6fy6sKc09jZ4hKH5T'
+# STRIPE_API_KEY = 'sk_test_Po5fLfcGq5FnakXbyvB7IIO9'
+STRIPE_PUBLISHABLE_KEY = 'pk_test_kq2QUHQQbfbIiz6RdJEbj9DU'
+STRIPE_API_KEY = 'sk_test_TwoBPzByKz7FZ35aoeBlbuTl'
+
+
 
 # エラー「Site matching query does not exist. 」を解消する為
 SITE_ID=2
--- universear/production_settings.py
+++ universear/production_settings.py
@@ -18,8 +18,9 @@ SOCIAL_AUTH_TWITTER_SECRET = 'zodNRE2HNnaOQyQAzMyg9xPdA7UunVcVdXkElkTO4NaAwQYxya
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '110344682483-j6lbc554or0gnq3rd17o3k48i3cpbti6.apps.googleusercontent.com'
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'C9Qj9F_sy0J0LNwNrnTVha5V'
 
-STRIPE_API_KEY = 'sk_test_Po5fLfcGq5FnakXbyvB7IIO9'
-STRIPE_PUBLISHABLE_KEY = 'pk_test_Qf9hdfU6fy6sKc09jZ4hKH5T'
+# STRIPE 決済設定
+STRIPE_PUBLISHABLE_KEY = 'pk_live_g1yb1Y7ka4ScPM8DnQiyPf6b'
+STRIPE_API_KEY = 'sk_live_CRTqfz7N4dC4b0CUFSFUzR2z'
 
 # エラー「Site matching query does not exist. 」を解消する為
 SITE_ID=2
