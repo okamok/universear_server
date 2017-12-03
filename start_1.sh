1e7a320a4f1bf5c05680761666aae565bff82c22
masahiro_mac <okamok108@gmail.com>
2017-12-03 14:02:42 +0900

    english response

--- start_1.sh
+++ start_1.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker-machine start ; eval "$(docker-machine env default)" ; cd /Users/user/develop/docker_python_nginx2;docker stop d61 5e1 ; docker-compose start ; docker exec -it 14b78b01cfda /bin/bash
+docker-machine start ; eval "$(docker-machine env default)" ; cd /Users/user/develop/universear;docker stop 3e2 7fd ; docker-compose start ; docker exec -it 14b78b01cfda /bin/bash
 
 # nginx -s stop
 # python manage.py runserver 0.0.0.0:8000
