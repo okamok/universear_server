de58af56cfae992722466afd687d9dea00b65def
masahiro_mac <okamok108@gmail.com>
2017-12-29 21:09:15 +0900

    target delete shell

---
+++ hlar/management/commands/deltemptarget.py
@@ -0,0 +1,31 @@
+from django.core.management.base import BaseCommand, CommandError
+import hlar.views as views
+from time import sleep
+from hlar.models import User, Target
+from datetime import datetime, timedelta
+
+class Command(BaseCommand):
+    help = 'delete Vuforia Target'
+
+    # def add_arguments(self, parser):
+    #     parser.add_argument('vuforia_target_id', nargs='+', type=str)
+
+    def handle(self, *args, **options):
+        print("del temp target")
+        #### 簡易登録で10分以上経過したものを取得
+        date = datetime.now() - timedelta(minutes=1)
+        targets_object = Target.objects.filter(user_id__isnull=True,created_date__lte=date, del_flg=0)
+
+        print(targets_object)
+
+        for target in targets_object:
+            print(target)
+
+            #### 削除処理
+            ret = views.del_target_func(target)
+
+            if (ret['ret'] == False):
+                # @ToDo 失敗ログ出力
+                print('err del')
+            else:
+                print('del comp')
