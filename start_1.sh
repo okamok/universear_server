ab63c88a3f807be91cd30cf02083d0907df82e42
masahiro_mac <okamok108@gmail.com>
2018-01-28 10:15:47 +0900

    URL定義を修正

--- start_1.sh
+++ start_1.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker-machine start ; eval "$(docker-machine env default)" ; cd /Users/user/develop/universear;docker stop e81 d61 ; docker-compose start ; docker exec -it 352 /bin/bash
+docker-machine start ; eval "$(docker-machine env default)" ; cd /Users/user/develop/universear;docker stop 3e2 ae4 ; docker-compose start ; docker exec -it 352 /bin/bash
 
 # nginx -s stop
 # python manage.py runserver 0.0.0.0:8000
