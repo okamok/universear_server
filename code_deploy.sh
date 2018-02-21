c1a05ee63d48b142c90f71debb6b977ce87fab93
masahiro_mac <okamok108@gmail.com>
2018-02-21 20:17:06 +0900

    code deploy shellを作成

---
+++ code_deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#git pull ;nginx -s stop ;kill ;nginx ; uwsgi --ini /code/mysite_uwsgi.ini
+
+
+git pull
+nginx -s stop
+pkill -f 'uwsgi'
+nginx
+uwsgi --ini /code/mysite_uwsgi.ini
+
+
+# nginx -s stop
+# python manage.py runserver 0.0.0.0:8000
